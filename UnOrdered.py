4d2b3070cb82f81630f7e9a30db30c9d1b6836e0
Osnaldy Vasquez <osnaldy@hotmail.com>
2017-07-06 19:02:37 -0400

    created an add method to add Node objects to the list

--- UnOrdered.py
+++ UnOrdered.py
@@ -1,3 +1,6 @@
+from MainNode import Node
+
+
 class UnOrderedList:
 
     def __init__(self):
@@ -8,3 +11,11 @@ class UnOrderedList:
 
         return self.head is None
 
+    def add(self, item):
+        # Creates a new Node and places the item as its data
+        temp = Node(item)
+        # Then we need to link the new Node into the existing structure
+        # Here we change the next reference of the new Node to refer to the old first Node in the list
+        temp.set_next(self.head)
+        # Then the head of the list is modify in order to refer to the new Node
+        self.head = temp
