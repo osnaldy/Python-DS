ed48cf04c2e561623d090ca006e5a3e39a5dc0c5
Osnaldy Vasquez <osnaldy@hotmail.com>
2017-07-18 16:56:59 -0400

    created function to reverse string of the given string

---
+++ recursive_functions.py
@@ -0,0 +1,33 @@
+# -----------Created a function to add recursively -----------------
+def list_sum(num_list):
+
+    if len(num_list) == 1:
+        return num_list[0]
+    else:
+        return num_list[0] + list_sum(num_list[1:])
+
+print(list_sum([1, 4, 5, 2, 3]), "is the sum of all the numbers")
+
+
+# -----------Created a function to convert integer to string of any base -----------------
+def to_string(n, base):
+
+    convert_string = "0123456789ABCDEF"
+
+    if n < base:
+        return convert_string[n]
+    else:
+        return to_string(n // base, base) + convert_string[n % base]
+
+print(to_string(10, 2))
+
+
+# -----------Created a function to add recursively -----------------
+def reverse_string(string):
+
+    if len(string) == 1:
+        return string
+    else:
+        return reverse_string(string[1:]) + string[0]
+
+print(reverse_string('Hello World'))
