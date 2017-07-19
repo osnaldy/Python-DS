8ba3f1b5724955170d85bc39bf651588542c2de4
Osnaldy Vasquez <osnaldy@hotmail.com>
2017-07-19 00:43:51 -0400

    Created a function to draw spiral using turtle module

--- visualizing.py
+++ visualizing.py
@@ -1,16 +1,20 @@
 import turtle
 
 my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
 
 my_win = turtle.Screen()
 
 
 def draw_spiral(my_turtle, line_len):
 
+    # Forward moves in pixels
+    # Left and right moves in degrees
+
     if line_len > 0:
         my_turtle.forward(line_len)
-        my_turtle.right(90)
+        my_turtle.left(90)
         draw_spiral(my_turtle, line_len - 5)
 
-draw_spiral(my_turtle, 200)
+draw_spiral(my_turtle, 100)
 my_win.exitonclick()
