29fa0808265e272303be503c1b9ac44c4d1f33b9
Osnaldy Vasquez <osnaldy@hotmail.com>
2017-07-26 20:04:08 -0400

    Created a function that draws a Fractal tree using turtle

--- FractalTree.py
+++ FractalTree.py
@@ -4,11 +4,11 @@ import turtle
 def tree(branch_len, t):
     if branch_len > 5:
         t.forward(branch_len)
-        t.right(20)
+        t.right(30)
         tree(branch_len - 15, t)
-        t.left(40)
+        t.left(60)
         tree(branch_len - 15, t)
-        t.right(20)
+        t.right(30)
         t.backward(branch_len)
 
 
@@ -16,13 +16,12 @@ def main():
 
     t = turtle.Turtle()
     my_win = turtle.Screen()
-    t.width(5)
     t.left(90)
     t.up()
     t.backward(100)
     t.down()
     t.color("black")
-    tree(75, t)
+    tree(85, t)
     my_win.exitonclick()
 
 main()
