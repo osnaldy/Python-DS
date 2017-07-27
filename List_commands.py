05bc0f3e0b54ad966103e7220dbff65524aaea80
Osnaldy Vasquez <osnaldy@hotmail.com>
2017-07-27 17:17:54 -0400

    List Comprehension for 3D grid Display

--- List_commands.py
+++ List_commands.py
@@ -13,3 +13,21 @@ for _ in range(n):
     else:
 
         print(l)
+
+x = int(input())
+y = int(input())
+z = int(input())
+num = int(input())
+ar = []
+p = 0
+
+print([[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n ])
+
+for i in range(x + 1):
+    for j in range(y + 1):
+        for k in range(z + 1):
+            if i + j + k != num:
+                ar.append([])
+                ar[p] = [i, j, k]
+                p += 1
+print(ar)
