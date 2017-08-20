009a6202d86845ed78d9dbd10f4c93a2f180238c
osnaldy <osnaldy@hotmail.com>
2017-08-20 10:24:25 -0400

    Created Another Approach Selection Sort Algorithm

--- SelectionSort.py
+++ SelectionSort.py
@@ -18,3 +18,21 @@ num = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 selection_sort(num)
 print(num)
 
+
+# Another way to approach this problem of Selection Sort, is by finding the minimum
+def selection_sort_algorithm(a_list):
+
+    for i in range(len(a_list)):
+
+        min_index = i
+
+        for j in range(i + 1, len(a_list)):
+
+            if a_list[min_index] > a_list[j]:
+
+                min_index = j
+        a_list[i], a_list[min_index] = a_list[min_index], a_list[i]
+
+num1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+selection_sort_algorithm(num1)
+print(num1)
