a39dbbd98335dae848e8949395b2295aa385d2ce
osnaldy <osnaldy@hotmail.com>
2017-09-01 21:50:08 -0400

    Created Another Approach Selection Sort Algorithm

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,17 +1,17 @@
 def selection_sort(a_list):
 
-    for fill_slot in range(len(a_list) - 1, 0, - 1):
+    for fill_slots in range(len(a_list) - 1, 0, - 1):
 
         max_index = 0
 
-        for location in range(1, fill_slot + 1):
+        for i in range(1, fill_slots + 1):
 
-            if a_list[location] > a_list[max_index]:
+            if a_list[i] > a_list[max_index]:
 
-                max_index = location
+                max_index = i
 
-        temp = a_list[fill_slot]
-        a_list[fill_slot] = a_list[max_index]
+        temp = a_list[fill_slots]
+        a_list[fill_slots] = a_list[max_index]
         a_list[max_index] = temp
 
 num = [54, 26, 93, 17, 77, 31, 44, 55, 20]
