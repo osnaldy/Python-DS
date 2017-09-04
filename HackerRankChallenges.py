cb7e8f0b0c8868438530e2ec0197a167be515c15
osnaldy <osnaldy@hotmail.com>
2017-09-03 21:16:25 -0400

    Function to find the Winner of the Game

--- HackerRankChallenges.py
+++ HackerRankChallenges.py
@@ -34,3 +34,53 @@ j = 5
 
 find_odd(l, j)
 
+# Game that find who is the winner between two players using the Odd or Even index of an array
+# HackerRank Challenge
+
+
+def find_winner(andrea, maria, s):
+
+    count_a = 0
+    count_m = 0
+    a = ""
+
+    if s == "Even":
+        a = andrea[::2]
+        m = maria[::2]
+        print(a)
+        print(m)
+
+        for i, j in zip(a, m):
+
+            count_a += i - j
+            count_m += j - i
+
+        if count_a > count_m:
+            a = "Andrea"
+        elif count_m > count_a:
+            a = "Maria"
+        else:
+            a = "Tie"
+
+    elif s == "Odd":
+        a = andrea[1::2]
+        m = maria[1::2]
+        print(a)
+        print(m)
+
+        for i, j in zip(a, m):
+            count_a += i - j
+            count_m += j - i
+
+        if count_a > count_m:
+            a = "Andrea"
+        elif count_m > count_a:
+            a = "Maria"
+        else:
+            a = "Tie"
+    return a
+
+andr = [1, 3, 4, 1, 1, 2, 1]
+mar = [7, 6, 3, 6, 1, 7, 2]
+
+print(find_winner(andr, mar, "Odd"))
