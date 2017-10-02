63804086167e6368ea41c55b518640495b6d1998
osnaldy <osnaldy@hotmail.com>
2017-10-01 21:45:30 -0400

    Created a linear search function

--- BasicArrayManipulation.py
+++ BasicArrayManipulation.py
@@ -26,8 +26,27 @@ class ArrayClass:
             return self.the_array[index]
         return 0
 
+    def linear_search(self, item):
+
+        value_in_array = False
+        index_with_value = ""
+
+        print("The value was found in the following index(s)")
+
+        for i in range(self.array_size):
+
+            if self.the_array[i] == item:
+                value_in_array = True
+                index_with_value += str(i) + " "
+
+        if not value_in_array:
+            index_with_value = "None"
+
+        return index_with_value
+
 array = ArrayClass()
 
 array.generate_random_array()
 array.print_array()
-print(array.get_value_at_index(3))
+print(array.get_value_at_index(1))
+print(array.linear_search(12))
