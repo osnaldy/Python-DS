063df6781ab296da6ea9a234854b81c7a4d60a43
osnaldy <osnaldy@hotmail.com>
2017-10-30 23:45:16 -0400

    Convert a number in the range of 0 to 999 to its string value

---
+++ ConvertNumberToItsStringValue.py
@@ -0,0 +1,60 @@
+ones = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen",
+        "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+
+tens = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+
+
+def convert_integer_to_string(n):
+
+    word = ""
+
+    if n == 0:
+
+        word = "Zero"
+
+    elif n <= 19:
+
+        word += ones[n - 1]
+
+    elif n <= 99:
+
+        word += tens[n // 10 - 2]
+        n = n % 10
+
+        if n != 0:
+
+            word += " " + ones[n % 10 - 1]
+
+        else:
+
+            word += ""
+
+    elif n <= 999:
+
+        word += ones[n // 100 - 1] + " Hundred "
+        n = n % 100
+
+        if n == 0:
+
+            word += ""
+
+        elif n <= 19:
+
+            word += ones[n - 1]
+
+        elif n <= 99:
+
+            word += tens[n // 10 - 2] + " "
+            n = n % 10
+
+            if n != 0:
+
+                word += ones[n-1]
+
+            else:
+
+                word += ""
+
+    return word
+
+print(convert_integer_to_string(895))
